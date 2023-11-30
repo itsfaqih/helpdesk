0eaebe9ce937e9bef5363f1900027ff12c44aaf2
Faqih Muntashir <itsfaqih@gmail.com>
2023-12-01 00:07:01 +0700

    feat: add rounded content style

--- src/pages/app/app.root.tsx
+++ src/pages/app/app.root.tsx
@@ -77,58 +77,56 @@ export function AppRoot() {
   };
 
   return (
-    <div>
-      <FadeInContainer from="left">
-        <nav className="fixed top-0 left-0 hidden w-64 h-full px-4 bg-white border-r border-gray-300 lg:block">
-          <span className="block py-6 font-medium text-center">Helpdesk Management</span>
-          <ul className="flex flex-col gap-y-2 mt-4">
-            {mainMenus.map((menu) => (
-              <li key={menu.to}>
-                <MainMenuItem to={menu.to} icon={menu.icon} label={menu.label} />
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <Ark.Dialog open={isSidebarOpen} onOpenChange={({ open }) => setIsSidebarOpen(open)}>
-          <Ark.Portal>
-            <Ark.Dialog.Backdrop
-              className={cn(
-                'fixed inset-0 bg-gray-900/70 lg:hidden',
-                'data-[state=open]:animate-in data-[state=open]:fade-in data-[state=open]:duration-300 data-[state=open]:ease-out',
-                'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:duration-300 data-[state=closed]:ease-in',
-              )}
-            />
-            <Ark.Dialog.Positioner>
-              <Ark.Dialog.Content asChild>
-                <nav
-                  className={cn(
-                    'fixed top-0 left-0 z-10 w-64 h-full px-4 bg-white border-r border-gray-300 lg:hidden',
-                    'data-[state=open]:animate-in data-[state=open]:slide-in-from-left data-[state=open]:duration-300 data-[state=open]:ease-out',
-                    'data-[state=closed]:animate-out data-[state=closed]:slide-out-to-left data-[state=closed]:duration-300 data-[state=closed]:ease-in',
-                  )}
-                >
-                  <span className="block py-6 font-medium text-center">Helpdesk Management</span>
-                  <ul className="flex flex-col gap-y-2 mt-4">
-                    {mainMenus.map((menu) => (
-                      <li key={menu.to}>
-                        <MainMenuItem
-                          to={menu.to}
-                          icon={menu.icon}
-                          label={menu.label}
-                          onClick={() => setIsSidebarOpen(false)}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-              </Ark.Dialog.Content>
-            </Ark.Dialog.Positioner>
-          </Ark.Portal>
-        </Ark.Dialog>
-      </FadeInContainer>
-      <main className="lg:pl-64">
-        <FadeInContainer from="top">
-          <nav className="py-1.5 px-6 border-b border-gray-300">
+    <div className="bg-gray-50 min-h-screen">
+      <nav className="fixed top-0 left-0 hidden w-64 h-full px-4 bg-gray-50 lg:block">
+        <span className="block py-6 font-medium text-center">Helpdesk Management</span>
+        <ul className="flex flex-col gap-y-2 mt-4">
+          {mainMenus.map((menu) => (
+            <li key={menu.to}>
+              <MainMenuItem to={menu.to} icon={menu.icon} label={menu.label} />
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Ark.Dialog open={isSidebarOpen} onOpenChange={({ open }) => setIsSidebarOpen(open)}>
+        <Ark.Portal>
+          <Ark.Dialog.Backdrop
+            className={cn(
+              'fixed inset-0 bg-gray-900/70 lg:hidden',
+              'data-[state=open]:animate-in data-[state=open]:fade-in data-[state=open]:duration-300 data-[state=open]:ease-out',
+              'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:duration-300 data-[state=closed]:ease-in',
+            )}
+          />
+          <Ark.Dialog.Positioner>
+            <Ark.Dialog.Content asChild>
+              <nav
+                className={cn(
+                  'fixed top-0 left-0 z-10 w-64 h-full px-4 bg-white border-r border-gray-300 lg:hidden',
+                  'data-[state=open]:animate-in data-[state=open]:slide-in-from-left data-[state=open]:duration-300 data-[state=open]:ease-out',
+                  'data-[state=closed]:animate-out data-[state=closed]:slide-out-to-left data-[state=closed]:duration-300 data-[state=closed]:ease-in',
+                )}
+              >
+                <span className="block py-6 font-medium text-center">Helpdesk Management</span>
+                <ul className="flex flex-col gap-y-2 mt-4">
+                  {mainMenus.map((menu) => (
+                    <li key={menu.to}>
+                      <MainMenuItem
+                        to={menu.to}
+                        icon={menu.icon}
+                        label={menu.label}
+                        onClick={() => setIsSidebarOpen(false)}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </Ark.Dialog.Content>
+          </Ark.Dialog.Positioner>
+        </Ark.Portal>
+      </Ark.Dialog>
+      <div className="lg:pl-64 p-3 min-h-screen">
+        <main className="bg-white rounded-xl border border-gray-300 min-h-[calc(100vh-1.5rem)]">
+          <nav className="py-2.5 px-3 border-b border-gray-300">
             <div className="flex items-center justify-between container mx-auto">
               <div className="flex items-center gap-4">
                 <IconButton
@@ -144,21 +142,13 @@ export function AppRoot() {
                 </ul>
               </div>
               <Menu positioning={{ placement: 'bottom-end' }}>
-                <MenuTrigger asChild>
-                  <button className="flex items-center px-2.5 py-2 rounded-lg gap-x-2 hover:bg-gray-50 focus-visible:bg-gray-50 focus-visible:outline-brand-600">
-                    <Avatar>
-                      <AvatarImage src={undefined} />
-                      <AvatarFallback>
-                        {loggedInUser?.full_name ? getInitials(loggedInUser.full_name) : ''}
-                      </AvatarFallback>
-                    </Avatar>
-                    <div className="flex-col hidden text-left w-36 sm:flex">
-                      <span className="text-sm font-medium text-gray-800 truncate">
-                        {loggedInUser?.full_name}
-                      </span>
-                      <span className="text-xs text-gray-600 truncate">{loggedInUser?.email}</span>
-                    </div>
-                  </button>
+                <MenuTrigger className="focus-visible:outline-brand-600 rounded-full">
+                  <Avatar>
+                    <AvatarImage src={undefined} />
+                    <AvatarFallback>
+                      {loggedInUser?.full_name ? getInitials(loggedInUser.full_name) : ''}
+                    </AvatarFallback>
+                  </Avatar>
                 </MenuTrigger>
                 <MenuContent>
                   <MenuItem id="profile">Profile</MenuItem>
@@ -169,13 +159,13 @@ export function AppRoot() {
               </Menu>
             </div>
           </nav>
-        </FadeInContainer>
-        <div className="p-6">
-          <div className="container mx-auto">
-            <Outlet />
+          <div className="p-6">
+            <div className="container mx-auto">
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
       <Toaster />
     </div>
   );
@@ -195,7 +185,7 @@ function MainMenuItem({ icon: Icon, label, className, ...props }: MainMenuItemPr
           {
             'text-gray-500 hover:shadow-haptic-gray-300 hover:text-gray-600 active:bg-gray-50 active:shadow-haptic-gray-400':
               !isActive,
-            'text-white active:bg-brand-100 bg-haptic-brand-700 hover:bg-haptic-brand-600':
+            'text-gray-700 active:bg-haptic-gray-50 bg-haptic-white hover:shadow-haptic-gray-500 shadow-haptic-gray-400':
               isActive,
             'animate-pulse': isPending,
           },
